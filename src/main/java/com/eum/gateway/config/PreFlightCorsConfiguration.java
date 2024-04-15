fec2ad07c92acce6543a24bf0653c5c977371778
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-04-16 04:51:14 +0900

    fix : cors 해결

--- src/main/java/com/eum/gateway/config/PreFlightCorsConfiguration.java
+++ src/main/java/com/eum/gateway/config/PreFlightCorsConfiguration.java
@@ -17,7 +17,7 @@ import reactor.core.publisher.Mono;
 public class PreFlightCorsConfiguration {
     private static final String ALLOWED_HEADERS = "x-requested-with, Authorization, Content-Type";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-    private static final String ALLOWED_ORIGIN = "http://localhost:3000";
+    private static final String ALLOWED_ORIGIN = "http://localhost:3000, https://hanmaeul.vercel.app";
     private static final String MAX_AGE = "3600";
 
     // true 로 설정할 경우 allowed_origin 에 '*' 을 입력할 수 없고,
@@ -36,7 +36,6 @@ public class PreFlightCorsConfiguration {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
                 headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Origin", "https://hanmaeul.vercel.app");
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
                 headers.add("Access-Control-Max-Age", MAX_AGE);
                 headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
