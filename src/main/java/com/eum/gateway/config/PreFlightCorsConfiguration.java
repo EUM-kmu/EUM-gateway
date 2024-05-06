67f82bd5a0d7f499309005d6d7e6126a92e9d6b9
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-07 03:20:15 +0900

    fix : 헤더허용

--- src/main/java/com/eum/gateway/config/PreFlightCorsConfiguration.java
+++ src/main/java/com/eum/gateway/config/PreFlightCorsConfiguration.java
@@ -19,7 +19,7 @@ import java.util.Set;
 @Configuration
 public class PreFlightCorsConfiguration {
 
-    private static final String ALLOWED_HEADERS = "x-requested-with, Authorization, Content-Type";
+    private static final String ALLOWED_HEADERS = "*";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS,PATCH";
     private static final String MAX_AGE = "3600";
     private static final String ALLOWED_CREDENTIALS = "true";
