af838389404787748464ba42c85182f0181731c0
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-10 02:29:07 +0900

    Chore : Docker

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-port=`lsof -i :80 -t`
+#port=`lsof -i :8080 -t`
 
-kill -9 $port
-git checkout dev
-git pull origin dev
-nohup ./gradlew bootRun > gradlew.log 2>&1 &
-
+#kill -9 $port
+git checkout server/deploy
+git pull origin server/deploy
+cp src/main/resources/devapplication.yml src/main/resources/application.yml
+./gradlew build -x test
+docker-compose up build -d
+#nohup ./gradlew bootRun > gradlew.log 2>&1 &
